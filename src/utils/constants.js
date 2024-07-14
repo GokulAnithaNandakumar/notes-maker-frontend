cf7e1ffd908a3bc74cf45b868cdca0336f6e1b5c
GokulAnithaNandakumar <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2024-07-14 14:27:47 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 // src/utils/constants.js
-export const BASE_URL = 'http://localhost:8000'; // Ensure this is correct for your environment
+export const BASE_URL = 'https://notes-app-backend-ntpt.onrender.com'; // Ensure this is correct for your environment
